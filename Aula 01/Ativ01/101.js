20696be7c8d7988b944936c3b5df20942fa3e111
phrochaf <phrochaf@gmail.com>
2024-05-13 21:24:38 -0300

    Rename

--- Aula 01/Ativ01/101.js
+++
@@ -1,4 +0,0 @@
-number = prompt("Insira o número:")
-if(number%2==0 && number>0){
-    console.log("Par")
-}else(console.log("Ímpar"))
---
+++ Aula 02/Ativ01/101.js
@@ -0,0 +1,4 @@
+number = prompt("Insira o número:")
+if(number%2==0 && number>0){
+    console.log("Par")
+}else(console.log("Ímpar"))
